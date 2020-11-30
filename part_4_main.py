dbbc49a35fd6e3d931231d2b8a19826613bf7248
Shadowguy1809 <guy.hadas01@gmail.com>
2020-11-30 16:14:47 +0200

    fixed

--- part_4_main.py
+++ part_4_main.py
@@ -1,5 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from part 2 main import *
+from part_2_main import *
+from part_3_main import *
+from euler_rocket import *
+
+x_0 = 0
+y_0 = 0
+dt = 0.0001
+v_0 = 350
+theta0 = 50
+friction = 0.0003348
+
+
+def hit_location(theta):
+    x_array, y_array = kassam_in_air(dt, x_0, y_0, v_0 * np.cos(theta * np.pi / 180),
+                                     v_0 * np.sin(theta * np.pi / 180), friction)
+    return x_array[-1]
+
+
+def function_to_find_root(theta, x_dest):
+    return x_dest - hit_location(theta)
+
+
+def secant_iteration(theta_n_minus_1, theta_n, f_n_minus_1, f_n):
+    theta_n_plus_1 = theta_n - f_n * (theta_n - theta_n_minus_1) / (f_n - f_n_minus_1)
+    return theta_n_plus_1
+
+
+def secant_loop(x_dest):
+    theta_n_minus_1 = 0.01
+    theta_n = 89.99
+    f_values = [hit_location(theta_n_minus_1), hit_location(theta_n)]
+    n = 1
+    while np.abs(theta_n - theta_n_minus_1) > 0.01:
+        theta_n_plus_1 = secant_iteration(theta_n_minus_1, theta_n, f_values[n-1], f_values[n])
+        theta_n_minus_1 = theta_n
+        theta_n = theta_n_plus_1
+        f_values.append(function_to_find_root(theta_n, x_dest))
+        n += 1
+    return theta_n
+
+
+print(secant_loop(3418.4854))
+
 
 

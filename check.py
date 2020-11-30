74f78ca14549a26de05377f69b7235a5f82fbb67
rann1234 <rann.shikler.42@gmail.com>
2020-11-30 14:42:41 +0200

    updates connection

--- check.py
+++ check.py
@@ -21,7 +21,7 @@ def kassam_in_vaccum(dt, x_0, y_0, v_x0, v_y0):
     while y_n > 10 ** (-6):
         x.append(x_n)
         y.append(y_n)
-        x_n, y_n, v_xn, v_yn = part_one_rocket_iteration()
+        x_n, y_n, v_xn, v_yn = part_two_rocket_iteration()
     return x, y
 
 

fa0de602295bbc0462ea54528619b0a69bd52baf
Omer Israeli <israeli.omer@gmail.com>
2020-11-30 14:58:36 +0200

    debuging

--- check.py
+++ check.py
@@ -2,7 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from euler_rocket import *
 
-
+x=0
+y=0
+dt=0.0001
+v0=1150
+theta0=50
 
 
 def kassam_in_vaccum(dt, x_0, y_0, v_x0, v_y0):
@@ -12,13 +16,19 @@ def kassam_in_vaccum(dt, x_0, y_0, v_x0, v_y0):
     v_yn = v_y0
     x = []
     y = []
-    while y_n > 10 ** (-6):
+    while y_n > 10 ** (-9) or len(y)<2:
         x.append(x_n)
         y.append(y_n)
-        x_n, y_n, v_xn, v_yn = part_two_rocket_iteration()
+        x_n, y_n, v_xn, v_yn = part_two_rocket_iteration(x_n,y_n,v_xn,v_yn,dt)
     return x, y
 
 if __name__ == '__main__':
-    kassam_in_vaccum()
+    v_x0 = v0*np.cos(theta0*np.pi / 180.)
+    v_y0 = v0*np.sin(theta0*np.pi / 180.)
+
+    x,y = kassam_in_vaccum(dt,x,y,v_x0,v_y0)
+    part_two_visualisation(x,y,dt)
+    print(x[-1])
+
 
 

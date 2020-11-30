e39236a29e0decae3d7326fb8615d40aa0245901
rann1234 <rann.shikler.42@gmail.com>
2020-11-30 14:26:30 +0200

    updates connection

--- check.py
+++ check.py
@@ -1,6 +1,3 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-print("Guy is in the git project, update if you see this and write below")
-print("ran was here")
-print("Guy likes to eat " + str(np.pi))

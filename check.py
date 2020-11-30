c28491f390d33071ba1f3487a1d3246964816612
Shadowguy1809 <guy.hadas01@gmail.com>
2020-11-30 13:57:18 +0200

    pipipi

--- check.py
+++ check.py
@@ -1,2 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 print("Guy is in the git project, update if you see this and write below")
 print("ran was here")
+print("Guy likes to eat " + str(np.pi))

1d34e5f0f9124fe75ad1561e21f047aed48f251c
rann1234 <rann.shikler.42@gmail.com>
2020-11-30 15:55:53 +0200

    updates connection

--- part_3_main.py
+++ part_3_main.py
@@ -19,26 +19,36 @@ def kassam_in_air (dt , x_0 , y_0 , vx_0 , vy_0, friction_coefficient):
     vy_n = vy_0
     x = []
     y = []
-    while y_n > 10 ** (-9) or len(y)<2:
+    while y_n > 10 ** (-9) or len(y) < 2:
         x.append(x_n)
         y.append(y_n)
         x_n, y_n, vx_n, vy_n = part_three_rocket_iteration(x_n, y_n, vx_n, vy_n, dt, friction_coefficient)
     return x, y
 
 
+
+def visualisation(xA, yA, dt, name):
+    t_array = np.array([n * dt for n in range(len(xA))])
+    x_array = np.array(xA)
+    y_array = np.array(yA)
+    plt.plot(x_array, y_array)
+    plt.xlabel(r'$x$ [$\mathrm{m}$]', size=15)
+    plt.ylabel(r'$y$ [$\mathrm{m}$]', size=15)
+    plt.title(name)
+    plt.grid()
+    plt.show()
+
+
 if __name__ == '__main__':
     v_x0 = v0*np.cos(theta0*np.pi / 180.)
     v_y0 = v0*np.sin(theta0*np.pi / 180.)
 
-    x,y = kassam_in_air(dt,x,y,v_x0,v_y0, friction)
+    xA,yA = kassam_in_air(dt,x,y,v_x0,v_y0, friction)
+    visualisation(xA,yA,dt,"Part 3 - Rocket launch in air")
+    xA, yA = kassam_in_air(dt, x, y, v_x0, v_y0, 0)
+    visualisation(xA, yA, dt, "Part 3 - Rocket launch in vacuum")
+    xA, yA = kassam_in_air(dt, x, y, v_x0, v_y0, 1)
+    visualisation(xA, yA, dt, "Part 3 - Rocket launch in thick air")
 
-    t_array = np.array([n * dt for n in range(len(x))])
-    x_array = np.array(x)
-    y_array = np.array(y)
-    plt.plot(x_array, y_array)
-    plt.xlabel(r'$x$ [$\mathrm{m}$]', size=15)
-    plt.ylabel(r'$y$ [$\mathrm{m}$]', size=15)
-    plt.title('Part 3 - Rocket launch in vacuum')
-    plt.grid()
-    plt.show()
-    print(x[-1])
+
+

c4c04147e1f53ef517f75ad5fba662e79c9858f5
Shadowguy1809 <guy.hadas01@gmail.com>
2020-11-30 14:42:59 +0200

    fixed

--- euler_rocket.py
+++ euler_rocket.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 G = 9.81
 
 
-def part_one_rocket_iteration(x_n, y_n, vx_n, vy_n, dt):
+def part_two_rocket_iteration(x_n, y_n, vx_n, vy_n, dt):
     x_n1 = x_n + dt * vx_n
     y_n1 = y_n + dt * vy_n
     vx_n1 = vx_n + dt * 0
@@ -14,5 +14,14 @@ def part_one_rocket_iteration(x_n, y_n, vx_n, vy_n, dt):
     return x_n1, y_n1, vx_n1, vy_n1
 
 
-def part_one_visualisation(x, y, dt):
-    t_array = np.array(dt)
+def part_two_visualisation(x, y, dt):
+    t_array = np.array([n * dt for n in range(len(x))])
+    x_array = np.array(x)
+    t_array = np.array(y)
+    plt.plot(x_array, y_array)
+    plt.legend(['A army', 'B army'])
+    plt.xlabel(r'$x$ $\mathrm{m]$', size=15)
+    plt.ylabel(r'$y$ $\mathrm{m]$', size=15)
+    plt.title('Part 2 - Rocket launch in vacuum.')
+    plt.grid()
+    plt.show()
